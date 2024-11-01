baf09ddf47e81f811de47a2597266f083c36b0a0
Mohammed Majid <majidmohammed044@gmail.com>
2024-11-01 22:09:01 +0000

    Added Hero section with interactivity

---
+++ interactive.js
@@ -0,0 +1,38 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+
+// Hero Section Title Animation...
+const titles = ["UI & UX Designer", "Web Developer", "Graphic Designer", "Frontend Developer"];
+let index = 0;
+const dynamicTitle = document.querySelector(".dynamic-title");
+
+function typeTitle() {
+    dynamicTitle.textContent = ""; // Clear previous title
+    let charIndex = 0;
+    const typingInterval = setInterval(() => {
+        dynamicTitle.textContent += titles[index][charIndex];
+        charIndex++;
+        if (charIndex === titles[index].length) {
+            clearInterval(typingInterval);
+            setTimeout(() => deleteTitle(), 2000);
+        }
+    }, 100); 
+}
+
+function deleteTitle() {
+    let charIndex = titles[index].length - 1;
+    const deletingInterval = setInterval(() => {
+        dynamicTitle.textContent = titles[index].substring(0, charIndex);
+        charIndex--;
+        if (charIndex < 0) {
+            clearInterval(deletingInterval);
+            index = (index + 1) % titles.length; // Move to the next title
+            setTimeout(typeTitle, 500); // Wait before typing next title
+        }
+    }, 50); 
+}
+
+typeTitle(); // Start the typing effect
